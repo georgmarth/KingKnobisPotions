e425b8ccc920ec7e57ae8ca489bc3e70d1ed76a7
heathmov <mjlopezeg@gmail.com>
2021-01-30 19:45:50 +0100

    no message

---
+++ Assets/Scripts/Cauldron/Cauldron.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour {
+
+    private Potion _potion;
+
+
+    public void NewPotion(Recipe recipe) { //subscribe this to recipe created
+
+    }
+
+
+
+    public void FlushPotion() { //subscribe to this flushevent -- automatic or by player button
+
+    }
+}
