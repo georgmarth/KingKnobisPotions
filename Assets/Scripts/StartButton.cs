2c5eb4e76dbb8be22427fa323f57bc265820deea
Georg Marth <georg.marth@kolibrigames.com>
2021-01-30 23:34:08 +0100

    UI Improvements

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
-    [SerializeField] private GameObject _startScreen;
-    
     public void OnStartButtonClick()
     {
         MainGameLoop.Instance.StartGame();
-        _startScreen.SetActive(false);
     }
 }
