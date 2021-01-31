bdb2027d03707be19ba85ccd71e872f50d2568b2
heathmov <mjlopezeg@gmail.com>
2021-01-31 11:17:41 +0100

    cauldron effects

--- Assets/Scripts/Cauldron/CauldronEffects.cs
+++ Assets/Scripts/Cauldron/CauldronEffects.cs
@@ -4,21 +4,32 @@ using Random = UnityEngine.Random;
 
 public class CauldronEffects : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioSource;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private EffectMapping[] _effectMappings;
     [SerializeField] private EffectMapping _successEffect;
     [SerializeField] private EffectMapping _failureEffect;
     [SerializeField] private EffectMapping _idleEffect;
-    
+
+    [SerializeField] private Material _bubbleMaterial;
+
     private void Start()
     {
         MessageBus.Instance.Subscribe<PotionCorrectEvent>(evt => PotionCorrect(evt.Potion));
         MessageBus.Instance.Subscribe<DropIngredientEvent>(evt => NewIngredient(evt.Ingredient));
+        MessageBus.Instance.Subscribe<WrongIngredientEvent>(evt => WrongIngredient());
+        MessageBus.Instance.Subscribe<NewRecipeEvent>(evt => FreshPotion());
     }
 
     public void FreshPotion() 
     {
-        _renderer.material.color = Color.cyan;
+        //stop VfX
+        _renderer.color = _idleEffect.Color;
+        _bubbleMaterial.color = _idleEffect.Color;
+        _idleEffect.ParticleEffect.Play();
+        
+
+        
     }
 
 
@@ -27,13 +38,25 @@ public class CauldronEffects : MonoBehaviour
         var correctMapping = _effectMappings.First(mapping => mapping.IngredientData == ingredient.IngredientData);
         if (correctMapping != null)
         {
-            // do effect
+            if (correctMapping.Color != null) {
+                _renderer.material.color = correctMapping.Color;
+                _bubbleMaterial.color = correctMapping.Color;
+            }
+            if (correctMapping.SoundEffect != null) {
+                
+            }
+            if (correctMapping.ParticleEffect != null) {
+
+            }
         }
-        _renderer.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 
     public void PotionCorrect(Potion potion) {
+        
+    }
 
+    public void WrongIngredient() {
+        
     }
 }
 
@@ -43,4 +66,5 @@ public class EffectMapping
     public IngredientData IngredientData;
     public AudioClip SoundEffect;
     public ParticleSystem ParticleEffect;
+    public Color Color;
 }
