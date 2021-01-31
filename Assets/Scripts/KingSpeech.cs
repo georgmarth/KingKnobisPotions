98d3460acdb05767c929e021b0998bcfe0ee2d3f
Georg Marth <georg.marth@kolibrigames.com>
2021-01-31 16:00:53 +0100

    add narration with music attenuation

---
+++ Assets/Scripts/KingSpeech.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class KingSpeech : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _mixer;
+    [SerializeField] private AudioSource _kingSpeech;
+
+    private void Start()
+    {
+        MessageBus.Instance.Subscribe<GameState>(OnGameStateChanged);
+    }
+
+    private void OnGameStateChanged(GameState state)
+    {
+        if (state == GameState.TitleScreen)
+        {
+            _mixer.SetFloat("musicVol", -25f);
+            _kingSpeech.Play();
+        }
+        else
+        {
+            _mixer.SetFloat("musicVol", -10f);
+            _kingSpeech.Stop();
+        }
+    }
+}
